4eaf7a8534745cfc041041fdd0f548d2063e4e61
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-16 04:31:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species from characters;"
+  "SELECT characters.species COUNT(*) from characters ;"
 end
 
 def select_name_and_series_subgenres_of_authors
